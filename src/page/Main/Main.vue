e906244f13edd8ecb5645c4a4666812121014faa
Kenneth <kenneth9417@gmail.com>
2024-03-13 08:11:25 +0800

    wip: fcp plan section

--- src/page/Main/Main.vue
+++ src/page/Main/Main.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="u-flex u-flex-col u-justify-start u-h100%">
+  <div
+    class="u-flex u-flex-col u-justify-start u-h100% u-mxauto"
+    :class="{ 'u-w1440px': 'lg' }"
+  >
     <FCPCarousel />
     <section
       name="main-content"
       class="u-flex u-flex-row u-justify-center u-items-start u-flex-nowrap u-h100%"
     >
-      <!-- <div style="height: 100%; width: max-content">
+      <div style="height: 100%; width: max-content">
         <Vue3Marquee
           :vertical="true"
           :gradient="true"
           :gradient-color="[255, 255, 255]"
           gradient-length="30%"
-          :clone="true"
-          :duration="6"
-          :direction="'reverse'"
-          :animate-on-overflow-only="true"
-          class="u-overflow-hidden"
         >
           <span
             class="left-tags u-font-unbounded u-c-fcp-gray u-text-60px u-text-nowrap"
@@ -23,10 +21,11 @@
             Visioning &#8226; Partnering &#8226; Fulfilling &#8226;
           </span>
         </Vue3Marquee>
-      </div> -->
+      </div>
       <section class="u-w-100%">
         <MainDescription />
         <FCPSchoolInfo />
+        <FCPPlan />
       </section>
     </section>
     <img :src="CTA" class="u-w-100%" />
@@ -35,11 +34,13 @@
 
 <script setup lang="js">
 
+import { Vue3Marquee } from 'vue3-marquee'
+
 import FCPCarousel from '@/components/FCPCarousel.vue'
 import CTA from '@/assets/img/CTA-image.png'
 import MainDescription from '@/components/MainDescription.vue'
 import FCPSchoolInfo from '@/components/FCPSchoolInfo.vue'
-import { Vue3Marquee } from 'vue3-marquee'
+import FCPPlan from '@/components/FCPPlan.vue'
 </script>
 
 <style lang="scss" scoped>
