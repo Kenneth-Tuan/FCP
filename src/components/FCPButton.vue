d76f5d497ffa0db1f19e8cdefc95982a9bcd95ec
Kenneth <kenneth9417@gmail.com>
2024-03-13 08:11:25 +0800

    wip: fcp plan section

---
+++ src/components/FCPButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <q-btn push square>
+      <slot></slot>
+    </q-btn>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.q-btn {
+  height: 45px;
+  padding: 0 24px;
+  background-color: #aec9c2;
+  color: white;
+  border-radius: 35px 0 35px 0;
+  font-size: 16px;
+  font-weight: 600;
+}
+</style>
---
+++ src/components/FCPPlan.vue
@@ -0,0 +1,52 @@
+<script setup>
+import FCPButton from "./FCPButton.vue";
+</script>
+
+<template>
+  <div class="u-border-black">
+    <div class="">
+      <FCPIcon
+        name="FCP-mark(o)"
+        class="u-w-84px u-h-84px u-absolute u-left--12px u-top--24px"
+      />
+      <span>工人靈命塑造</span>
+    </div>
+
+    <div class="">
+      <FCPIcon name="FCP-icon-Spiritual Life and Character Formation" />
+      <span>跨文化認識</span>
+    </div>
+
+    <div class="">
+      <FCPIcon name="FCP-icon-Language Learning" />
+      <span>語言學習與操練</span>
+    </div>
+
+    <div class="">
+      <FCPIcon name="FCP-icon-Developing Ministries" />
+      <span>穆宣事工發展</span>
+    </div>
+
+    <div class="">
+      <FCPIcon name="FCP-icon-Missional Team Development" />
+      <span>宣教團隊建造</span>
+    </div>
+
+    <div class="">
+      <FCPIcon name="FCP-icon-Disciple Making" />
+      <span>一對一教練</span>
+    </div>
+
+    <div class="">
+      <FCPIcon name="FCP-icon-Overseas internship" />
+      <span>海外實習</span>
+    </div>
+
+    <FCPButton>
+      看更多内容
+      <FCPIcon name="arrow_forward" />
+    </FCPButton>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
