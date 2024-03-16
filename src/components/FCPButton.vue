3220061f111d4f5f0bc869cc63d975f5d03cf346
Kenneth <kenneth9417@gmail.com>
2024-03-16 15:47:18 +0800

    feat: main page

--- src/components/FCPButton.vue
+++ src/components/FCPButton.vue
@@ -1,21 +1,20 @@
 <template>
-  <div>
-    <q-btn push square>
-      <slot></slot>
-    </q-btn>
-  </div>
+  <q-btn push square>
+    <slot></slot>
+  </q-btn>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
 .q-btn {
-  height: 45px;
+  height: 60px;
+  width: 300px;
   padding: 0 24px;
   background-color: #aec9c2;
   color: white;
-  border-radius: 35px 0 35px 0;
-  font-size: 16px;
-  font-weight: 600;
+  border-radius: 45px 0 45px 0;
+  // font-size: 24px;
+  // font-weight: 600;
 }
 </style>
