c7c33e30919789d04ff1646815c8fe3254c27e8d
Kenneth <kenneth9417@gmail.com>
2024-04-07 22:27:40 +0800

    feat: api infra

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useUserStore } from "@/stores/user";
 import { responseErrorHandler } from "@/utils/axios/utils";
 
-const baseURL = import.meta.env.VITE_FWB_API_BASE_URL;
+const baseURL = import.meta.env.VITE_FCP_API_BASE_URL;
 
 export const apiHelper = axios.create({
   baseURL,
@@ -37,35 +37,3 @@ apiHelper.interceptors.response.use(
 export const apiHelper_auth = axios.create({
   baseURL: import.meta.env.VITE_AUTH_API_BASE_URL,
 });
-
-export const apiHelper_ezycargo = axios.create({
-  baseURL: import.meta.env.VITE_EZYCARGO_API_BASE_URL,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
-
-apiHelper_ezycargo.interceptors.request.use(
-  function (config) {
-    const userStore = useUserStore();
-    const { getToken } = userStore;
-
-    const token = getToken();
-    config.headers.Authorization = `Bearer ${token}`;
-
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
-
-apiHelper_ezycargo.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    responseErrorHandler(error);
-  }
-);
