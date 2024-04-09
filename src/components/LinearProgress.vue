738fb8b3985f7a9f421fe4ae1e620771b8c2248c
Kenneth-Tuan <pokemenggou0.0@gmail.com>
2024-04-09 10:02:24 +0800

    wip: liner progress component

---
+++ src/components/LinearProgress.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <q-linear-progress :value="progress" color="secondary" />
+  </div>
+</template>
+
+<script setup>
+import { ref, onBeforeUnmount, onMounted } from "vue";
+
+const progress = ref(0);
+
+let interval;
+
+onMounted(() => {
+  interval = setInterval(() => {
+    progress.value = progress.value + 0.025;
+  }, 80);
+});
+
+onBeforeUnmount(() => {
+  clearInterval(interval);
+});
+</script>
+
+<style lang="scss" scoped></style>
