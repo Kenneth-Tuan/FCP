eb046e8823852300563848fe8db966bd650dbdf3
Kenneth <kenneth9417@gmail.com>
2024-04-17 23:46:12 +0800

    style: adjust fcp school

--- src/components/FCPSchoolInfo.vue
+++ src/components/FCPSchoolInfo.vue
@@ -3,78 +3,68 @@
     class=""
     :class="{
       'u-bg-fcp-green-1': 'color',
-      'u-py48px u-px36px': 'spacing',
-      'u-rounded-72px': 'shape',
+      'u-p5% ': 'spacing',
+      'lg:u-rounded-72px md:u-rounded-60px sm:u-rounded-48px': 'shape',
       'u-flex u-flex-col u-flex-gap-y-36px': 'layout',
       'u-relative': 'position',
     }"
   >
     <FCPIcon
       name="FCP-mark(green)"
-      class="u-w108px u-h108px u-absolute u-left-80px u-top-54px"
+      class="u-w108px u-h108px u-absolute u-left-80px u-top--54px"
       fill="u-c-fcp-green-1"
     />
 
-    <div class="">
-      <p class="u-c-fcp-blue u-text-48px u-font-heavy u-pl-72px">
+    <div class="u-c-fcp-blue">
+      <p class="lg:u-text-48px md:u-text-36px sm:u-text-24px u-font-heavy">
         前線穆宣工人訓練學校
       </p>
-      <p class="u-c-fcp-blue u-pl-72px u-text-24px">
+      <p class="lg:u-text-24px md:u-text-12px">
         *亦稱「前線穆宣訓練中心」/「穆訓」
       </p>
     </div>
 
-    <div class="u-pl-84px u-relative">
-      <FCPIcon
-        name="FCP-mark(blue)"
-        class="u-w-84px u-h-84px u-absolute u-left--12px u-top--24px"
-      />
-      <div class="u-flex u-flex-row u-flex-nowrap u-items-center">
-        <P class="u-c-fcp-blue u-text-24px u-font-heavy u-mr-24px">FCP的使命</P>
-        <q-separator class="u-flex-1 u-bg-fcp-blue" size="1.5px" />
-      </div>
-      <p class="u-c-white u-text-36px u-lh-36px u-font-unbounded">
-        Our Mission
-      </p>
-    </div>
+    <FCPSubtitle subtitleMsg="FCP的使命" description="Our Mission" />
 
-    <div class="u-flex u-flex-row u-pl-72px">
-      <div class="u-h-180px u-w-180px u-mr-48px">
-        <img :src="FCP_LOGO_CIRCLE" alt="" class="u-w-100% u-h-100%" />
+    <div class="u-flex u-justify-center u-items-center">
+      <div class="u-aspect-ratio-1 u-w20% u-min-w24px u-mr-48px">
+        <img :src="FCP_LOGO_CIRCLE" alt="" class="u-w100% u-aspect-ratio-1" />
       </div>
-      <div class="u-flex u-flex-col u-justify-between u-items-start u-flex-1">
+      <div
+        class="u-flex u-flex-col u-justify-center u-flex-gap-y-10% u-items-start"
+      >
         <div>
           <p class="u-c-black u-text-24px u-font-bold">
-            <span class="u-c-fcp-blue">裝備</span>並<span class="u-c-fcp-blue">
-              差派
-            </span>
-            基督的門徒進入穆宣禾場，
-          </p>
-          <p class="u-c-black u-text-24px u-font-bold">
-            <span class="u-c-fcp-blue"> 以愛和尊重 </span>
-            邀請穆斯林群體跟隨耶穌基督。
+            <span class="u-c-fcp-blue">裝備</span>並<span class="u-c-fcp-blue"
+              >差派</span
+            >基督的門徒進入穆宣禾場，<br class="lg:u-block sm:u-hidden" /><span
+              class="u-c-fcp-blue"
+              >以愛和尊重</span
+            >邀請穆斯林群體跟隨耶穌基督。
           </p>
         </div>
 
-        <p class="u-c-white u-text-18px u-font-unbounded u-pr-108px">
-          Equip and empower disciples of Jesus to invite all Muslim peoples,
+        <p class="u-c-white u-text-18px u-font-unbounded">
+          Equip and empower disciples of Jesus to invite all Muslim peoples,<br
+            class="lg:u-block sm:u-hidden"
+          />
           with love and respect to follow Jesus.
         </p>
       </div>
     </div>
 
     <div
-      class="u-bg-fcp-gray u-h-max u-flex u-flex-row u-justify-between u-px-48px u-rounded-[96px_0_96px_0] u-ml-84px u-mr-60px"
+      class="u-w100% u-bg-fcp-gray u-flex u-justify-between u-px5% u-rounded-[60px_0]"
     >
-      <div class="u-w-30% u-flex u-flex-row u-items-center u-justify-center">
-        <img :src="FTWSB_LOGO" alt="" class="u-w-80% u-h-80%" />
+      <div class="u-w30% u-flex u-flex-row u-items-center u-justify-center">
+        <img :src="FTWSB_LOGO" alt="" class="u-w80% u-h-80%" />
       </div>
       <q-separator spaced inset vertical color="white" size="2px" />
-      <div class="u-w-30%">
+      <div class="u-w30%">
         <img :src="HLLLT_LOGO" alt="" class="u-w-100% u-h-100%" />
       </div>
       <q-separator spaced inset vertical color="white" size="2px" />
-      <div class="u-w-30% u-flex u-flex-row u-items-center u-justify-center">
+      <div class="u-w30% u-flex u-flex-row u-items-center u-justify-center">
         <img :src="BIRS_LOGO" alt="" class="u-w-80% u-h-80%" />
       </div>
     </div>
@@ -84,54 +74,28 @@
       description="Our Program Descripion"
     />
 
-    <div class="u-grid u-grid-cols-4 u-grid-gap-36px">
-      <div class="u-col-span-1 u-rounded-[72px_0_0_0] u-overflow-hidden">
-        <p
-          class="u-text-center u-bg-fcp-yellow u-font-bold u-text-32px u-pb16px"
+    <div
+      :class="{
+        'u-flex u-flex-gap-x-24px u-justify-center': 'layout',
+        'u-font-bold lg:u-text-32px md:u-text-24px sm:u-text-14px': 'text',
+      }"
+    >
+      <div
+        v-for="item in programDescription"
+        :class="{
+          'md:u-rounded-[72px_0_0_0] sm:u-rounded-[16px_0_0_0]': 'shape',
+          'u-flex u-flex-col u-justify-start u-items-start': 'layout',
+          'u-overflow-hidden': 'display',
+        }"
+      >
+        <div class="u-overflow-hidden u-flex u-items-center u-justify-center">
+          <img :src="item.img" alt="" class="u-w100%" />
+        </div>
+        <div
+          class="u-w100% u-bg-fcp-yellow u-text-center u-flex u-items-center u-justify-center lg:u-p16px md:u-p8px sm:u-p4px"
         >
-          <img
-            :src="PROGRAM_DESCRIPTION_1"
-            alt=""
-            class="u-w100% u-h100% u-mb16px"
-          />
-          發展以實戰培訓<br />訓練模式
-        </p>
-      </div>
-      <div class="u-col-span-1 u-rounded-[72px_0_0_0] u-overflow-hidden">
-        <p
-          class="u-text-center u-bg-fcp-yellow u-font-bold u-text-32px u-pb16px"
-        >
-          <img
-            :src="PROGRAM_DESCRIPTION_2"
-            alt=""
-            class="u-w100% u-h100% u-mb16px"
-          />
-          建立師徒傳承的<br />生命關係
-        </p>
-      </div>
-      <div class="u-col-span-1 u-rounded-[72px_0_0_0] u-overflow-hidden">
-        <p
-          class="u-text-center u-bg-fcp-yellow u-font-bold u-text-32px u-pb16px"
-        >
-          <img
-            :src="PROGRAM_DESCRIPTION_3"
-            alt=""
-            class="u-w100% u-h100% u-mb16px"
-          />
-          建構跨越環境的<br />使徒團隊
-        </p>
-      </div>
-      <div class="u-col-span-1 u-rounded-[72px_0_0_0] u-overflow-hidden">
-        <p
-          class="u-text-center u-bg-fcp-yellow u-font-bold u-text-32px u-pb16px"
-        >
-          <img
-            :src="PROGRAM_DESCRIPTION_4"
-            alt=""
-            class="u-w100% u-h100% u-mb16px"
-          />
-          突破伊斯蘭處境<br />建立教會
-        </p>
+          {{ item.title }}
+        </div>
       </div>
     </div>
 
@@ -140,21 +104,39 @@
       description="Why Choose FCP?"
     />
 
-    <div class="u-flex u-flex-row u-justify-center u-flex-gap-x-24px">
+    <div
+      class="u-flex u-flex-row u-justify-center u-items-center u-flex-gap-x-24px"
+    >
       <div
         v-for="item in contentOfWhyChooseFcp"
         :key="item.month"
-        class="u-w20% u-p24px u-aspect-ratio-1 u-bg-fcp-black u-rounded-25px u-flex u-flex-col u-items-center u-justify-start u-group hover:u-bg-fcp-blue u-transition-all u-duration-300ms u-ease-in-out"
+        class="u-p4% u-w25% u-aspect-ratio-1 u-overflow-hidden u-bg-fcp-black u-rounded-[15%]"
+        :class="{
+          'u-flex u-flex-col u-items-center u-justify-center': 'layout',
+          ' u-group hover:u-bg-fcp-blue u-transition-all u-duration-300ms u-ease-in-out':
+            'animation',
+        }"
       >
         <p class="u-c-fcp-green">
-          <span class="u-text-48px">{{ item.month }}</span>
-          <span class="u-text-30px">個月</span>
+          <span
+            :class="{ 'lg:u-text-48px md:u-text-32px sm:u-text-16px': 'text' }"
+            >{{ item.month }}</span
+          >
+          <span
+            class="u-text-32px"
+            :class="{ 'lg:u-text-32px md:u-text-22px sm:u-text-12px': 'text' }"
+            >個月</span
+          >
         </p>
-        <span
-          class="u-c-white u-text-48px u-text-center group-hover:u-font-heavy u-transition-all u-duration-300ms u-ease-in-out"
+        <p
+          class="u-c-white u-text-center"
+          :class="{
+            'lg:u-text-48px md:u-text-32px sm:u-text-12px': 'text',
+            'group-hover:u-font-heavy u-transition-all u-duration-300ms u-ease-in-out':
+              'animation',
+          }"
           v-html="item.description"
-        >
-        </span>
+        ></p>
       </div>
     </div>
   </div>
@@ -187,6 +169,25 @@ const contentOfWhyChooseFcp = [
   {
     month: "1",
     description: "回顧<br />加強",
+  },
+];
+
+const programDescription = [
+  {
+    title: "發展以實戰培訓訓練模式",
+    img: PROGRAM_DESCRIPTION_1,
+  },
+  {
+    title: "建立師徒傳承的生命關係",
+    img: PROGRAM_DESCRIPTION_2,
+  },
+  {
+    title: "建構跨越環境的使徒團隊",
+    img: PROGRAM_DESCRIPTION_3,
+  },
+  {
+    title: "突破伊斯蘭處境建立教會",
+    img: PROGRAM_DESCRIPTION_4,
   },
 ];
 </script>
--- src/components/FCPSubtitle.vue
+++ src/components/FCPSubtitle.vue
@@ -6,20 +6,27 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="u-pl-84px u-relative">
+  <div class="lg:u-pl84px md:u-pl72px sm:u-pl60px u-relative">
     <FCPIcon
       name="FCP-mark(blue)"
-      class="u-w-84px u-h-84px u-absolute u-left--12px u-top--24px"
+      :class="{
+        'u-absolute lg:u-top--24px sm:u-left--12px sm:u-top--20px': 'position',
+        'lg:u-w84px md:u-w72px sm:u-w60px': 'dimension',
+        'u-aspect-ratio-1': 'layout',
+      }"
     />
     <div class="u-flex u-flex-row u-flex-nowrap u-items-center">
-      <P
-        class="u-c-fcp-blue u-text-24px u-font-heavy u-mr-24px u-tracking-0.3em"
+      <span
+        class="u-c-fcp-blue u-font-heavy u-mr-24px u-tracking-0.3em"
+        :class="{
+          'lg:u-text-24px sm:u-text-18px': 'text',
+        }"
       >
         {{ subtitleMsg }}
-      </P>
+      </span>
       <q-separator class="u-flex-1 u-bg-fcp-blue" size="1.5px" />
     </div>
-    <p class="u-c-gray-1 u-text-32px u-lh-24px u-font-heavy">
+    <p class="u-c-gray-1 lg:u-text-32px md:u-text-26px u-lh-24px u-font-heavy">
       {{ description }}
     </p>
   </div>
