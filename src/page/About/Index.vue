8a16118ff3f493e21f2e94e813958eedd3bbb85d
Kenneth-Tuan <pokemenggou0.0@gmail.com>
2024-05-28 10:11:01 +0800

    wip: about page

--- src/page/About/Index.vue
+++ src/page/About/Index.vue
@@ -45,26 +45,69 @@
         </div>
       </div>
 
-      <div class="">
+      <div class="u-flex u-flex-col u-justify-start u-items-center">
         <FCPSubtitle
           subtitleMsg="核心價值"
           description="Core Values"
-          class="u-mb36px"
+          class="u-mb36px u-w100%"
         />
-        <div class="u-grid u-grid-flow-row">
+        <div
+          class="u-flex u-justify-start u-flex-gap-x-24px u-flex-gap-y-16px u-flex-wrap"
+        >
           <div
+            v-for="(coreValueInfo, index) in coreValuesInfo"
             class="u-rounded-16px u-overflow-hidden u-c-fcp-gray"
             :class="[
-              { 'u-bg-fcp-green': true },
+              'u-w255px',
               'u-text-center',
-              'u-py2% u-px5%',
+              'u-py8px u-px24px',
+              `${(index + 1) % 2 === 0 ? 'u-bg-fcp-blue' : 'u-bg-fcp-green'}`,
             ]"
           >
-            <p class="u-fw700 u-text-24px u-c-fcp-blue">title</p>
-            <q-separator inset class="u-bg-fcp-gray" />
-            <p class="u-fw600 u-text-18px">description</p>
+            <p
+              class="u-fw700 u-text-24px"
+              :class="[
+                `${(index + 1) % 2 === 0 ? 'u-c-fcp-green' : 'u-c-fcp-blue'}`,
+              ]"
+            >
+              {{ coreValueInfo.title }}
+            </p>
+            <q-separator inset class="u-bg-fcp-gray u-my8px" />
+            <p class="u-fw600 u-text-18px u-break-words">
+              {{ coreValueInfo.description }}
+            </p>
           </div>
         </div>
+      </div>
+
+      <div class="">
+        <FCPSubtitle
+          subtitleMsg="訓練特色"
+          description="Our Feature"
+          class="u-mb36px u-w100%"
+        />
+        <div class="u-flex u-flex-col u-flex-gap-16px">
+          <div
+            :class="[
+              'u-flex',
+              {
+                'sm:u-flex-col md:u-flex-row u-flex-gap-16px': 'layout',
+              },
+            ]"
+          >
+            <q-img :src="Feature1" />
+            <q-img :src="Feature2" />
+          </div>
+          <q-img :src="Feature3" />
+        </div>
+      </div>
+
+      <div class="">
+        <FCPSubtitle
+          subtitleMsg="師資團隊"
+          description="Our Mentors"
+          class="u-mb36px u-w100%"
+        />
       </div>
     </div>
   </div>
@@ -73,14 +116,17 @@
 <script setup lang="js">
 import FCPSubtitle from "@/components/FCPSubtitle.vue";
 import Banner from "@/assets/img/about/about_page.png";
-import FCPButton from "@/components/FCPButton.vue";
+import Feature1 from "@/assets/img/about/實體全修班｜七個月課程.jpeg"
+import Feature2 from "@/assets/img/about/FCP課程介紹.jpeg"
+import Feature3 from '@/assets/img/about/FCP五大面向課程.jpeg'
 
-const coreValuesList = [
+
+const coreValuesInfo = [
   {title: '專注', description: '專注於穆斯林歸主運動，致力在群體中建立教會。'},
   {title: '專業', description: '專業訓練裝備工人能力，穆宣禾場有效完成任務。'},
   {title: '品格', description: '看重工人生命品格門訓，工人品格與事奉生命。'},
-  {title: '專業', description: '專業訓練裝備工人能力，穆宣禾場有效完成任務。'},
-  {title: '專業', description: '專業訓練裝備工人能力，穆宣禾場有效完成任務。'},
+  {title: '團隊', description: '看重工人恩慈發展配搭，恩慈相待的團隊關係。'},
+  {title: '夥伴', description: '看重與教會、神學院、機構、工場團隊夥伴關係。'},
 ]
 </script>
 
